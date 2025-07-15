5f6d9bdefbc0f194d3b024359f4e2ee6d84d8874
Arnav Anand <beingarnavanand@gmail.com>
2025-07-15 21:30:29 +0530

    Add files via upload

---
+++ smart-gpt/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Smart-GPT | Your AI Assistant",
+  description: "A custom-built GPT chatbot that interacts like a human Created by Arnav",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
